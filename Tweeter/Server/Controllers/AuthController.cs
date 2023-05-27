46cbdf5260096ee41b6f6be897a50124317ab646
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-05-27 12:01:09 +0200

    finished login page, added register page

--- Tweeter/Server/Controllers/AuthController.cs
+++ Tweeter/Server/Controllers/AuthController.cs
@@ -108,14 +108,4 @@ namespace Tweeter.Server.Controllers
     }
 }
 
-public class RegisterRequest
-{
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; } = null!;
-    [Required]
-    public string Username { get; set; } = null!;
-    [Required]
-    public string Password { get; set; } = null!;
-}
 
---
+++ Tweeter/Shared/Models/RegisterRequest.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterRequest
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+    [Required]
+    public string Username { get; set; } = null!;
+    [Required]
+    public string Password { get; set; } = null!;
+}
